4becbd6ca77ad7b891702e74849d1d7389b47921
Ivan Ivanov <ivan41ds@gmail.com>
2022-12-07 15:39:27 +0300

    Добавил компоненты строки и клетки

---
+++ frontend/src/components/Cell/index.tsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+const Cell = () => {
+    return (
+        <div className="Cell"></div>
+    )
+};
+
+export default Cell
---
+++ frontend/src/components/Row/index.tsx
@@ -0,0 +1,16 @@
+import "./style.css";
+import Cell from "../Cell";
+
+const Row = () => {
+    return (
+        <div className="Row">
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+            <Cell />
+        </div>
+    )
+};
+
+export default Row
